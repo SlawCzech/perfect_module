89eaf085fa1f4bcf6015433c0314431a8aa0d055
SlawCzech <slaw.czech@gmail.com>
2022-10-17 22:27:34 +0200

    documentation using docstring

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
+
+"""Fetch and print words
+
+Usage:
+    python main.py <filepath>
+    ./main.py <filepath>
+
+    from main import main
+    main(<filepath>)
+
+"""
+
+
 import sys
 
 
 def fetch_words(filepath):
+    """
+    Fetch words from given filepath
+    :param filepath: relative or absolute filepath
+    :type filepath: str
+    :return: word list from given filepath
+    :rtype: list[str]
+    """
     with open(filepath, 'r') as file:
         return [word for line in file for word in line.split()]
         # words = []
@@ -13,14 +33,29 @@ def fetch_words(filepath):
         # return words
 
 
-def main(filepath):
-    words = fetch_words(filepath)
-    print_items(words)
-
 
 def print_items(items):
+    """
+    Print items from iterable
+    :param items: any iterable collection of data
+    :type items: iterable
+    :return: None
+    :rtype: None
+    """
     for item in items:
         print(item)
+
+
+def main(filepath):
+    """
+    Fetch and show word from given filepath
+    :param filepath: relative or absolute filepath
+    :type filepath: str
+    :return: None
+    :rtype: None
+    """
+    words = fetch_words(filepath)
+    print_items(words)
 
 
 # read_file('text.txt')
@@ -30,8 +65,7 @@ def print_items(items):
 if __name__ == '__main__':
     # read_file('text.txt')
     # read_file(sys.argv[1])
-
-    main(sys.argv[1])
+    main(sys.argv[1])  # Zero-index is module name (indeks 0 to nazwa modułu)
 # print(__name__) dla shella: __main__
 # print(__name__) dla pythona: app
 
